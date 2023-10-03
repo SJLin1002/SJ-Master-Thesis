d00f1cb9e996aaf7bea0622eeac58ea0f8cf7b4c
SJLin1002 <95974441+SJLin1002@users.noreply.github.com>
2023-10-03 16:25:01 +0800

    Add files via upload

--- Code/text01.py
+++ Code/text01.py
@@ -1,79 +1,104 @@
-#car location 
-
 import random
-import matplotlib.pyplot as plt
-import keyboard
 import time
-import threading
+import copy
 
-running = True
 
-def checkKeyInput ():
-    global running
-    while (running):
-        if keyboard.read_key() == "p":
-            print("You pressed p")
-            running = False
+def create_car(car_num): 
+    data = []
+    for i in range(car_num):
+        # data增加car_count台車,[x,y,v]copy
+        data.append(
+            [random.randint(0, 100), random.choice((0, 3)), random.randint(15, 30)]
+            #車輛時速54~108km/hr(15~30m/s)
+        )
+    return data
 
-t = threading.Thread(target = checkKeyInput)
-t.start()
+def kmeans(data):
+    #kmeans 
+    # clusters=2，隨機生成兩點
+    node1=[round(random.uniform(0,100),3),round(random.uniform(0,3),3)]
+    node2=[round(random.uniform(0,100),3),round(random.uniform(0,3),3)]
+    while True:
+        clusA=[]
+        clusB=[]    
+        #車(data)到node1,node2的距離-分群
+        for i in range(len(data)):
+            d1=((data[i][0]-node1[0])**2+(data[i][1]-node1[1])**2)**0.5
+            d2=((data[i][0]-node2[0])**2+(data[i][1]-node2[1])**2)**0.5
+            if d1 < d2 :
+                clusA.append([data[i][0],data[i][1]])
+            else: 
+                clusB.append([data[i][0],data[i][1]])
+            # print("d1 :",d1)
+            # print("d2 :",d2)
+        #重心點
+        if len(clusA)==0 :
+            node1=[round(random.uniform(0,100),3),round(random.uniform(0,3),3)]
+            #print("A群=None")
+            continue
+        elif len(clusB)==0:
+            node2=[round(random.uniform(0,100),3),round(random.uniform(0,3),3)]
+            #print("B群=None")
+            continue
+        else:
+            Ax = 0
+            Ay = 0
+            Bx = 0
+            By = 0
 
-# # 按按鈕
-# while True:
-#     print("1")
-#     time.sleep(2)  
-#     if keyboard.read_key() == "p":
-#         print("You pressed p")
-#         break
+            for j in range(len(clusA)):
+                Ax += clusA[j][0]
+                Ay += clusA[j][1]
+            for k in range(len(clusB)):
+                Bx += clusB[k][0]
+                By += clusB[k][1]
 
-#車輛數X
-car_num=5
-car=[]
+            New_node1 = (round(Ax/len(clusA),3),round(Ay/len(clusA),3))
+            New_node2 = (round(Bx/len(clusB),3),round(By/len(clusB),3))
+         
+            if node1 == New_node1 and node2 == New_node2:
+                # print("A :",clusA,"Node1 :",New_node1)
+                # print("B :",clusB,"Node2 :",New_node2)
+                break
+            else:
+                node1=New_node1
+                node2=New_node2
+    # print("A :",clusA,"Node1 :",New_node1)
+    # print("B :",clusB,"Node2 :",New_node2) 
+    center_point = ((node1[0]+node2[0])/2,(node1[1]+node2[1])/2)    
+    return center_point       
 
-#車輛初始位置(x,y,v)
-for i  in range(1,car_num+1):
-    x = random.randint(0,100)
-    y = random.choice([0,3])
-    v = random.randint(20,100)
-    car.append([x,y,v])  # 將新的 [x, y, v] 值添加到 car 列表
-print(car_num,"台車" , "座標為" , car)
-t=1
-while running:
-    
-    for j  in range(0,car_num):    
-        car[j][0] = round((car[j][0] + car[j][2]),3)  #車輛X座標改變
+def simulation_change_car_data(dataS):
+    for car in dataS:
+        #車輛X座標等速改變
+        car[0] = round((car[0] + car[2]),3)        
+    return dataS
 
-        k = random.random()
-        if k > 0.2 :  #更改Y(車道) 
-            if car[j][1] == 0:
-                car[j][1] = 3
-            else: car[j][1] = 0        
-        a = random.uniform(0.1,-0.1) 
-        New_v = car[j][2] * (1+a)
-        car[j][2] = round(New_v,3) #更改車輛速度
-    print(car_num,"台車Time", t , "座標為" , car)
-    # if keyboard.read_key() == "p":
-    #     print("You pressed p")
-    #     break
-    time.sleep(1)
-    t+=1
-    
-print("A")
-        
 
-''' 
-V_UAV = (0,6) #無人機速度(y)
-V_CAR = (0,4) #車輛速度
-target=(0 ,10) #目標位置
-totalTime = 5 #花費時間(s)  
-location_UAV=[]
-location_CAR=[] 
-location_UAV.append((1,2)) 
-location_CAR.append((3,4)) 
-location_UAV.append((5,6)) 
-location_UAV[0]=(7,8)
-print(location_CAR)
-print(location_UAV)
-#S.T. d[k] / UAV_V <= k
 
-'''
+car_num = 100
+O_data=create_car(car_num)
+dataN = copy.deepcopy(O_data)
+
+center_point = kmeans(dataN)
+V_UAV = 35
+#無人機時速126km/hr(35m/s)
+location_UAV=(0,0,V_UAV)  #無人機初始位置
+d =((location_UAV[0]-center_point[0])**2+(location_UAV[1]-center_point[1])**2)**0.5
+t = d/35
+print("初始車輛位置 : ",O_data)
+print("初始無人機位置 : ",(location_UAV[0],location_UAV[1]))
+print("初始中心點 : ",center_point) #k1
+print("距離相差 : ",d)
+print("無人機飛行時間 :",t)
+
+for i in range(0,int(t/0.5)):
+    dataN = simulation_change_car_data(dataN)
+
+New_center = kmeans(dataN)
+print("實際車輛中心點為 : ",New_center)  #k2
+
+ans = ((New_center[0]-center_point[0])**2+(New_center[1]-center_point[1])**2)**0.5
+print("兩點相差距離 : ",ans)
+
+
